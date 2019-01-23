fcbf2896328825bc4ed64f7f12bb878496abbc9d
Jaime <jaime.glez.pacheco@gmail.com>
2019-01-23 16:54:32 -0500

    README updated

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to ngx-translate-cache-test!');
+    expect(page.getParagraphText()).toEqual('Welcome to ngx-translate-cache-playground!');
   });
 });
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ngx-translate-cache-test'`, () => {
+  it(`should have as title 'ngx-translate-cache-playground'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ngx-translate-cache-test');
+    expect(app.title).toEqual('ngx-translate-cache-playground');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ngx-translate-cache-test!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ngx-translate-cache-playground!');
   });
 });
